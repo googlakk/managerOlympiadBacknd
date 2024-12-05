6a684caf74e8d51fe591306584de3b5e25a9aa86
googlakk <googl.akk@mail.ru>
2024-12-05 09:48:32 +0600

    fix name _vercel_jwt

--- frontend/src/data/service/getToken.ts
+++ frontend/src/data/service/getToken.ts
@@ -3,6 +3,6 @@
 import { cookies } from "next/headers";
 
 export async function getAuthToken() {
-  const authToken = cookies().get("jwt")?.value;
+  const authToken = cookies().get("_vercel_jwt")?.value;
   return authToken;
 }

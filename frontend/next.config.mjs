0ce24ec98f10bca9244f002ec9dd67fc5a652bf5
googlakk <googl.akk@mail.ru>
2024-12-10 16:17:42 +0600

    productionBrowser

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -18,6 +18,7 @@ const nextConfig = {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY, // добавляем переменную окружения для API OpenAI
   },
   reactStrictMode: false,
+  productionBrowserSourceMaps: true
 };
 
 export default nextConfig;
